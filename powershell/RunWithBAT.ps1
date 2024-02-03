8c4dcbbd7be1640cb0c15bfea756d84793047805
Julek <31626592+dnjulek@users.noreply.github.com>
2024-02-02 22:17:14 -0300

    Fix dll name in ps1 file

--- powershell/RunWithBAT.ps1
+++ powershell/RunWithBAT.ps1
@@ -6,10 +6,10 @@ $zipUrl = $jsonContent.master.'x86_64-windows'.tarball
 $parentFolder = Split-Path -Path $PSScriptRoot -Parent
 $zipFilePath = Join-Path $PSScriptRoot "zig-windows-x86_64-$version.zip"
 $zigPath = Join-Path $PSScriptRoot "\zig-windows-x86_64-$version\zig.exe"
-$dllFilePath = Join-Path $parentFolder "\zig-out\lib\invert_example.dll"
+$dllFilePath = Join-Path $parentFolder "\zig-out\lib\ssimulacra2.dll"
 $destinationFileFolder = Join-Path $env:APPDATA "\VapourSynth\plugins64"
 New-Item -ItemType Directory -Force -Path $destinationFileFolder | Out-Null
-$destinationFilePath = Join-Path $destinationFileFolder "\invert_example.dll"
+$destinationFilePath = Join-Path $destinationFileFolder "\ssimulacra2.dll"
 
 if (-not (Test-Path $zipFilePath)){
     Write-Host "Downloading zig-windows-x86_64-$version.zip..." -ForegroundColor Green
@@ -21,12 +21,12 @@ if (-not (Test-Path $zigPath)){
     Expand-Archive -Path $zipFilePath -DestinationPath $PSScriptRoot -Force
 }
 
-Write-Host "Building invert_example.dll..." -ForegroundColor Green
+Write-Host "Building ssimulacra2.dll..." -ForegroundColor Green
 & $zigPath build -Doptimize=ReleaseFast
 
 if (Test-Path $dllFilePath){
-    Write-Host "Installing 'invert_example.dll' to '$destinationFilePath'" -ForegroundColor Blue
+    Write-Host "Installing 'ssimulacra2.dll' to '$destinationFilePath'" -ForegroundColor Blue
     Copy-Item -Path $dllFilePath -Destination $destinationFilePath -Force
 } else {
-    Write-Host "No '\zig-out\lib\invert_example.dll' file, build error." -ForegroundColor Red
+    Write-Host "No '\zig-out\lib\ssimulacra2.dll' file, build error." -ForegroundColor Red
 }
